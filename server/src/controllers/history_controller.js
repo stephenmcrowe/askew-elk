220d95e0f6eae7b77c2a26c5a9e8f0547e8b1656
JamesTann <jt.21@dartmouth.edu>
2020-05-21 18:26:26 -0400

    working on controllers

--- server/src/controllers/history_controller.js
+++ server/src/controllers/history_controller.js
@@ -5,15 +5,18 @@ export const getHistory = (req, res) => {
 
 };
 
-export const updateHistory = (req, res) => {
-
-};
-export const deleteHistory = (req, res) => {
-
-};
 export const getHistories = (req, res) => {
 
 };
+
+export const updateHistory = (req, res) => {
+
+};
+
+export const deleteHistory = (req, res) => {
+
+};
+
 export const createHistories = (req, res) => {
 
 };
--- server/src/controllers/user_controller.js
+++ server/src/controllers/user_controller.js
@@ -1,8 +1,31 @@
 import mysql from 'mysql';
 import db from '../db';
 
+const WHERE_ID = 'WHERE UserID = ?';
+const SELECT_ONE = ` SELECT UserName FROM users `;
+const SELECT_BY_ID = `${SELECT_ONE} ${WHERE_ID}`;
 export const getUser = (req, res) => {
+    db.query(SELECT_BY_ID, req.params.id)
+        .then((result) => {
+            console.log(result);
+            res.status(200).json({ error: null, response: result });
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json({ error: err.sqlMessage, response: null });
+        });
+};
 
+const SELECT_ALL = `SELECT UserID, UserName FROM users`;
+export const getUsers = (req, res) => {
+    db.query(SELECT_ALL)
+        .then((result) => {
+            res.status(200).json({ error: null, response: result });
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json({ error: err.sqlMessage, response: null });
+        });
 };
 
 export const updateUser = (req, res) => {
@@ -10,9 +33,5 @@ export const updateUser = (req, res) => {
 };
 
 export const deleteUser = (req, res) => {
-
-};
-
-export const getUsers = (req, res) => {
 
 };

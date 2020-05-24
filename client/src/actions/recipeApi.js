1471914110321c6a608d46ae1aef5f2e17823d54
Stephen Crowe <crowe.stephen.m@gmail.com>
2020-05-23 20:14:31 -0700

    Added a quick recipeApi

---
+++ client/src/actions/recipeApi.js
@@ -0,0 +1,47 @@
+import axios from 'axios';
+import { ActionTypes, ROOT_URL } from './index';
+
+export function getRecipe(id) {
+  return (dispatch) => {
+    return new Promise((resolve, reject) => {
+      const url = `${ROOT_URL}/recipe/${id}`;
+      console.log(`GET: ${url}`);
+      axios.get(url)
+        .then((resp) => {
+          const { response } = resp.data;
+          dispatch({ type: ActionTypes.FETCH_RECIPE, payload: response });
+          resolve(response);
+        })
+        .catch((error) => {
+          dispatch({ type: ActionTypes.ERROR_SET, error });
+          reject(error);
+        });
+    });
+  };
+}
+
+export function getRecipes(params) {
+  /*
+   * Params should look like:
+   * {
+   *    recipeName: str
+   *    recipeAuthor: username (str)
+   * }
+   */
+  return (dispatch) => {
+    return new Promise((resolve, reject) => {
+      const url = `${ROOT_URL}/recipes`;
+      console.log(`GET: ${url}`);
+      axios.get(url, { params })
+        .then((resp) => {
+          const { response } = resp.data;
+          dispatch({ type: ActionTypes.FETCH_RECIPES, payload: response });
+          resolve(response);
+        })
+        .catch((error) => {
+          dispatch({ type: ActionTypes.ERROR_SET, error });
+          reject(error);
+        });
+    });
+  };
+}
